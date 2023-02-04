8c99ea997aedd9dd85c6fe03bb87e497db6b8376
DiogoPedebos <diogo.iffar@gmail.com>
2023-02-04 20:41:01 -0300

    add conteudo de variaveis

--- tipo-variaveis/src/TipoVariaveis.java
+++ tipo-variaveis/src/TipoVariaveis.java
@@ -1,5 +1,23 @@
 public class TipoVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte exemplo1 = 127;
+        short exemplo2 = 32767;
+        int exemplo3 = 2147483647;
+        // long exemplo4 = 9223372036854775808; 
+
+        System.out.println(exemplo1 + " " + exemplo2 + " " + exemplo3 + " ");
+        System.out.println("float 4 bytes e double 8 bytes");
+        
+        // Armazenar valorem como cpf/rg/cep que comecem com 0 podem não ser armazenados corretamente:
+        long cpf = 02222222222;
+        System.out.println("\n" + cpf + " Primeira impressão do cpf\n");
+
+        // Possível solução:
+        String cpfTexto = "02222222222";
+        System.out.println(cpfTexto + " Segunda impressão do cpf\n");
+
+        // Exemplo de constante:
+        final double VALOR_DE_PI = 3.24;
+        System.out.println(VALOR_DE_PI);
     }
 }
